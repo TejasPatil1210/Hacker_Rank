0157ae0ceb19f005561eac5e7e3ac915957e929f
TejasPatil1210 <tejupatil1210@gmail.com>
2022-11-18 10:51:26 +0530

    Solution Uploaded

--- Java_DataTypes.java
+++ Java_DataTypes.java
@@ -1,5 +1,5 @@
 /*
-ava has 8 primitive data types; char, boolean, byte, short, int, long, float, and double. For this exercise, we'll work with the primitives used to hold integer values (byte, short, int, and long):
+Java has 8 primitive data types; char, boolean, byte, short, int, long, float, and double. For this exercise, we'll work with the primitives used to hold integer values (byte, short, int, and long):
 
 A byte is an 8-bit signed integer.
 A short is a 16-bit signed integer.
